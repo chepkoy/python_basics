cc0412c5c72ac8d8bf66ebceb05d5114c813dba4
chepkoy <invalskies12@gmail.com>
2017-10-29 18:01:05 +0300

    Naming variables in python

---
+++ names.py
@@ -0,0 +1,13 @@
+# Creating variables
+favourite_fruit = "apple"
+favourite_number = 42
+
+print(favourite_fruit)
+print(favourite_number)
+
+# Doing operations with variables
+print(favourite_number / 4)
+
+# Deleting variable completely
+del favourite_number
+print(favourite_number)
