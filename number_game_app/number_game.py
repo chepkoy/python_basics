17ec26a0fba63e54b2206a59b6b4b056b9015ea8
chepkoy <invalskies12@gmail.com>
2017-10-31 22:08:13 +0300

    Updated number game app

--- number_game_app/number_game.py
+++ number_game_app/number_game.py
@@ -1,16 +1,34 @@
 import random
 
-# Generate a number between 1 and 10
-secret_num = random.randint(1, 10)
+def game():
+    # Generate a number between 1 and 10
+    secret_num = random.randint(1, 10)
+    guesses =[]
 
-while True:
-    # Get a number guess from the player
-    guess = int(input("Guess a number between 1 and 10: "))
+    while len(guesses) < 5:
+        try:
+            # Get a number guess from the player
+            guess = int(input("Guess a number between 1 and 10: "))
+        except ValueError:
+            print(" {} is not a number! ".format(guess))
+        else:
+            # Compare guess to secret number
+            if guess == secret_num:
+                print("You got it! My number was {}".format(secret_num))
+                break
 
-    # Compare guess to secret number
-    if guess == secret_num:
-        print("You got it! My number was {}".format(secret_num))
-        break
+            elif guess < secret_num:
+                print("My number is higher than {} ".format(guess))
+
+            else:
+                print("My number is lower than {}".format(guess))
+            guesses.append(guess)
     else:
-        print("That's not it")
-    # print hit or miss
+        print("You didn't get it ! My number was {}".format(secret_num))
+
+    play_again = input("Do you want to play again? Y/n ")
+    if play_again.lower() !='n':
+        game()
+    else:
+        print("Bye:")
+game()
