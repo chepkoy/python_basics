dd5ed831ca487415169169c5c8efb3fb6ac1acf9
chepkoy <invalskies12@gmail.com>
2017-10-31 16:10:05 +0300

    created a folder and added booleans file on how to work with booleans in python

---
+++ logic/booleans.py
@@ -0,0 +1,23 @@
+true = True
+false = False
+
+print(True + True)
+
+# Bool function to test weather something is true or false
+
+print(bool(true))
+print(bool(false))
+
+# Empty datatypes are False
+
+print(bool([]))
+print(bool(''))
+
+# Datatypes containing  things Evaluate to True
+
+print(bool([1,2,3,4]))
+print(bool('allan'))
+
+# Nothingness
+
+print(bool(None))
