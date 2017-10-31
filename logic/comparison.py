fba3efee81c23dc63e292d54f1582ed1cad30dab
chepkoy <invalskies12@gmail.com>
2017-10-31 16:24:54 +0300

    Weighing options in python

---
+++ logic/comparison.py
@@ -0,0 +1,31 @@
+# Comparing values returns either true or false
+
+# Equal to for this case it returns true
+print(5==5)
+
+# Equal to for this case it returns False
+print(5 == 7)
+
+# Not equal to returns true
+
+print(5 != 7)
+
+# Not equal to returns false
+
+print(5 != 5)
+
+# Greater than and less than
+
+print(5 > 2)
+print(2 < 3)
+
+# Greater than or equal to and vice versa
+
+print(5 <= 7)
+print(10 >= 8)
+
+# Checking if a variable is none
+
+my_variable = None
+
+print(my_variable is None)
