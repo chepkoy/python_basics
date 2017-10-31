0561fbc725de77c11f5cc5d1496c07195b7130a1
chepkoy <invalskies12@gmail.com>
2017-10-31 15:44:02 +0300

    Indexes in python for mutable strings and lists

---
+++ index.py
@@ -0,0 +1,14 @@
+# Checking indexes of strings
+my_name = 'allan'
+print(my_name.index('a'))
+
+# Checking index in a list
+my_list = list('alpha')
+print(my_list.index('p'))
+
+#Getting something at a certain index
+
+print(my_name[4])
+print(my_list[1])
+
+
